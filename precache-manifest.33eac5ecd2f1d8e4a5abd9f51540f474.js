5f0ac25b77a76e5294827c3b48fc4f6661abec82
moraisandreua <48691769+moraisandreua@users.noreply.github.com>
2020-05-26 18:58:37 +0100

    Updates

---
+++ precache-manifest.33eac5ecd2f1d8e4a5abd9f51540f474.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "469a2c7107946ee120584b9c1faa5bcf",
+    "url": "/theWalkerPublic/index.html"
+  },
+  {
+    "revision": "50b1a6223728e04e13db",
+    "url": "/theWalkerPublic/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4fba6494608389d20eeb",
+    "url": "/theWalkerPublic/static/css/main.2b997481.chunk.css"
+  },
+  {
+    "revision": "50b1a6223728e04e13db",
+    "url": "/theWalkerPublic/static/js/2.eef88c85.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/theWalkerPublic/static/js/2.eef88c85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fba6494608389d20eeb",
+    "url": "/theWalkerPublic/static/js/main.193d3af3.chunk.js"
+  },
+  {
+    "revision": "55974ac8a40dc4fa2386",
+    "url": "/theWalkerPublic/static/js/runtime-main.5895d6b8.js"
+  },
+  {
+    "revision": "a227e5acc823e0e96eb836aec7c6d25a",
+    "url": "/theWalkerPublic/static/media/addCard.a227e5ac.png"
+  },
+  {
+    "revision": "0c4d2945a000c3746d2774cde90c42fb",
+    "url": "/theWalkerPublic/static/media/backImage.0c4d2945.png"
+  },
+  {
+    "revision": "6773036f5bcdc3ace7112394d17a8cae",
+    "url": "/theWalkerPublic/static/media/berkshireswash.6773036f.woff"
+  },
+  {
+    "revision": "1800d920c30a6d0d4806a1ea32ef69f2",
+    "url": "/theWalkerPublic/static/media/bg.1800d920.jpg"
+  },
+  {
+    "revision": "e2797ea007b36661ec8a57e964d4e3d4",
+    "url": "/theWalkerPublic/static/media/buy.e2797ea0.png"
+  },
+  {
+    "revision": "92934e9ec76a8ff82724f1d48b2909a4",
+    "url": "/theWalkerPublic/static/media/dog1.92934e9e.jpg"
+  },
+  {
+    "revision": "f8ce61d7752611d41fce1e224cbd5370",
+    "url": "/theWalkerPublic/static/media/dog2.f8ce61d7.jpg"
+  },
+  {
+    "revision": "ae8288e7c1067c8d092cf272a0a3a759",
+    "url": "/theWalkerPublic/static/media/dog3.ae8288e7.jpg"
+  },
+  {
+    "revision": "5d56228c3e2bc793754ce2365c6d0122",
+    "url": "/theWalkerPublic/static/media/dog4.5d56228c.jpg"
+  },
+  {
+    "revision": "feb6340cf8a6d044374c7169533957e2",
+    "url": "/theWalkerPublic/static/media/johnwick.feb6340c.jpg"
+  },
+  {
+    "revision": "9be03fccc8657fcc7819685131746b6c",
+    "url": "/theWalkerPublic/static/media/leash.9be03fcc.png"
+  },
+  {
+    "revision": "cedf2ac62e049e0c7357cb2337759064",
+    "url": "/theWalkerPublic/static/media/location.cedf2ac6.png"
+  },
+  {
+    "revision": "f7ce3d5a6b11f86d335a39805ba30248",
+    "url": "/theWalkerPublic/static/media/logo.f7ce3d5a.png"
+  },
+  {
+    "revision": "1d21c55beb8079f8c8a5ed96e62220fb",
+    "url": "/theWalkerPublic/static/media/luke.1d21c55b.png"
+  },
+  {
+    "revision": "5981c5bed7a89b2da4c5841f5e7d20f3",
+    "url": "/theWalkerPublic/static/media/mammal.5981c5be.png"
+  },
+  {
+    "revision": "378d670136deebfd8682605e0c23a06e",
+    "url": "/theWalkerPublic/static/media/paymentComplete.378d6701.png"
+  },
+  {
+    "revision": "45dd54703cf1537d439bc358c680762c",
+    "url": "/theWalkerPublic/static/media/rateStar.45dd5470.png"
+  },
+  {
+    "revision": "82d856dffa1b4ebeec71ab32d42b3fa9",
+    "url": "/theWalkerPublic/static/media/rateStarEscavada.82d856df.png"
+  },
+  {
+    "revision": "a45d76ea7f38fbdb26ce0e2aff2cdd81",
+    "url": "/theWalkerPublic/static/media/rocky.a45d76ea.jpg"
+  },
+  {
+    "revision": "7ce6c119f76025d2f505c42262f1408b",
+    "url": "/theWalkerPublic/static/media/social.7ce6c119.png"
+  },
+  {
+    "revision": "a139218c6fab83c1eefb3c83764b4ea3",
+    "url": "/theWalkerPublic/static/media/trela.a139218c.png"
+  },
+  {
+    "revision": "2ac944bce67608508bbc45bb7c3726c8",
+    "url": "/theWalkerPublic/static/media/user.2ac944bc.jpg"
+  },
+  {
+    "revision": "d385d7ffb327067893883431f2522ba9",
+    "url": "/theWalkerPublic/static/media/visa.d385d7ff.png"
+  }
+]);
